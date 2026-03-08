32587d449582c3284fe155cdd76cc7c01590221f
Admin <admin@MacBook-Pro-de-Admin.local>
2026-03-08 21:09:51 +0100

    chore: cleanup unnecessary files from firefox branch

--- push_to_github.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git remote set-url origin https://github.com/OuincheWinch/Challenge-Rank.git
-echo "Fetching changes from GitHub..."
-git fetch origin
-
-echo "Pushing code to GitHub (FORCE)..."
-# Using -f in case of history mismatch (e.g. repo created with README vs local)
-git push -f -u origin main
